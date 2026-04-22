b5cfd6ef304ed02b5808216be413994d370e526e
Amr Abdelwahab <amorenew@gmail.com>
2026-04-22 12:47:22 +0200

    Update Gradle and plugin configurations, add toolchain support, and adjust Flutter environment paths

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/amr/fvm/versions/stable"
-export "FLUTTER_APPLICATION_PATH=/Users/amr/Desktop/record_mp3_plus/example"
+export "FLUTTER_APPLICATION_PATH=/Users/amr/Desktop/private/programming/record_mp3_plus/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
